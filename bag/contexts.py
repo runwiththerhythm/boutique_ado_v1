c65bd246b3a421b938dfbf0fb39625820a0cbbb0
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-11-02 20:29:55 +0000

    Added adjust bag and remove from bag views and calc subtotal template filter

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,7 +11,7 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-     for item_id, item_data in bag.items():
+    for item_id, item_data in bag.items():
 
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
