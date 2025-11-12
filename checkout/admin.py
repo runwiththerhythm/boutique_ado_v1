586c858f5ea262798ca770f227302d9bf9f22ec3
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-11-12 19:45:31 +0000

    Completed webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,7 +12,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',)
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
@@ -22,8 +22,9 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total',)
+                    'grand_total', 'original_bag',)
 
     ordering = ('-date',)
 
 admin.site.register(Order, OrderAdmin)
+
